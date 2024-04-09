58cf8df595e5bff0670c8fe0374851812acbd1f7
PugzAreCute <me@pugzarecute.com>
2024-04-09 10:28:15 +0530

    spes: fix parts compilation for QPR2

--- parts/src/co/aospa/settings/display/OledFeaturesPreferenceActivity.java
+++ parts/src/co/aospa/settings/display/OledFeaturesPreferenceActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.widget.R;
+import com.android.settingslib.collapsingtoolbar.R;
 
 public class OledFeaturesPreferenceActivity extends CollapsingToolbarBaseActivity {
     @Override
